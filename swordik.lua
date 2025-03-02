b67a7360e6a96b68045098de57c17d78201d3015
Swordikk <133685730+Swordikk@users.noreply.github.com>
2025-03-02 13:03:17 +0400

    Update swordik.lua

--- swordik.lua
+++ swordik.lua
@@ -6,7 +6,16 @@ local localplayer = players.LocalPlayer
 local character = localplayer.Character
 local humanoid = character.Humanoid
 local humanoidrootpart = character.HumanoidRootPart
-
+local Thieving  = { 
+    Vector3.new(194.7723846435547, 5.393672466278076, -1.0488529205322266),
+    Vector3.new(220.02371215820312, 8.296595573425293, -10.974353790283203),
+    Vector3.new(240.34519958496094, 8.2985200881958, -13.118978500366211),
+    Vector3.new(274.0917663574219, 10.293213844299316, -41.420467376708984),
+    Vector3.new(295.7476501464844, 10.351471900939941, -44.3170051574707),
+    Vector3.new(321.22930908203125, 14.39346981048584, -23.170984268188477),
+    Vector3.new(335.78338623046875, 14.409186363220215, -2.5764825344085693),
+    Vector3.new(350.5539855957031, 19.969709396362305, 37.36346435546875)
+ }
 
 
 
@@ -45,44 +54,50 @@ function JumpPower()
     end
 end
 
-local function AutoFarmFruits()
-	while _G.AutoFarmFruits == true do wait(0.01)
-        for _, v in pairs(workspace.__THINGS.Breakables:GetChildren()) do
-            
-
-
-
-
-
-
-
-
-
+local function AutoFarmThieving()
+	while _G.AutoFarmThieving == true do
+        for i = 1, #Thieving do
+            humanoidrootpart.CFrame = CFrame.new(Thieving[i])
+            wait(5)
         end
 	end
 end
 
-local function AutoFarmGifts()
-	while _G.AutoFarmGifts == true do wait(0.01)
-        for _, v in pairs(workspace.__THINGS.HiddenGifts:GetChildren()) do
-            local PathfindingService = game:GetService("PathfindingService")
-			local path = PathfindingService:CreatePath()
-			path:ComputeAsync(humanoidrootpart.Position,v.Model.Position)
+local function AutoFarm()
+	while _G.AutoFarm == true do wait(0.01)
+        for _, folder in pairs(workspace.__THINGS.Breakables:GetChildren()) do
+            if folder:IsA("Model") then
+                for _, v in pairs(folder:GetChildren()) do
+                    if v:IsA("MeshPart") and v.Name == "base" then
+                        local PathfindingService = game:GetService("PathfindingService")
+                        local path = PathfindingService:CreatePath()
+                        path:ComputeAsync(humanoidrootpart.Position,v.Position)
 
-			for _, waypoint in pairs(path:GetWaypoints()) do
-				humanoid:MoveTo(waypoint.Position)
-				humanoid.MoveToFinished:Wait()
-			end
+                        for _, waypoint in pairs(path:GetWaypoints()) do
+                            humanoid:MoveTo(waypoint.Position)
+                            humanoid.MoveToFinished:Wait()
+                        end
+                    else
+                        mypath = false
+                    end
+                end
+            end
+        end
 
+        if mypath == false then
+            for _, v in pairs(workspace.__THINGS.HiddenGifts:GetChildren()) do
+                if v.Name == "Model" then
+                    local PathfindingService = game:GetService("PathfindingService")
+                    local path = PathfindingService:CreatePath()
+                    path:ComputeAsync(humanoidrootpart.Position,v.Model.Position)
 
-
-
-
-
-
-
-
-
+                    for _, waypoint in pairs(path:GetWaypoints()) do
+                        humanoid:MoveTo(waypoint.Position)
+                        humanoid.MoveToFinished:Wait()
+                    end
+                    mypath = true
+                end
+            end
         end
 	end
 end
@@ -100,20 +115,20 @@ local Tab = Window:MakeTab({
 })
 
 Tab:AddToggle({
-	Name = "AutoFarmFruits",
+	Name = "AutoFarmThieving",
 	Default = false,
 	Callback = function(Value)
-		_G.AutoFarmFruits = Value
-		AutoFarmFruits()
+		_G.AutoFarmThieving = Value
+		AutoFarmThieving()
 	end    
 })
 
 Tab:AddToggle({
-	Name = "AutoFarmGifts",
+	Name = "AutoFarm",
 	Default = false,
 	Callback = function(Value)
-		_G.AutoFarmGifts = Value
-		AutoFarmGifts()
+		_G.AutoFarm = Value
+		AutoFarm()
 	end    
 })
 
